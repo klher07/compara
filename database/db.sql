7b7a0525f876a5adb491af7b6a19752322b6d820
CarluisHernandez <klher07@gmail.com>
2021-03-07 19:15:59 -0500

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,21 +1,76 @@
--- to create a new database
-CREATE DATABASE crudnodejsmysql;
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-03-2021 a las 01:13:01
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.2.27
 
--- to use database
-use crudnodejsmysql;
-
--- creating a new table
-CREATE TABLE customer (
-  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  address VARCHAR(100) NOT NULL,
-  phone VARCHAR(15)
-);
-
--- to show all tables
-show tables;
-
--- to describe table
-describe customer;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `interview`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `sellIn` int(10) NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `sellIn`, `price`) VALUES
+(2, 'Medium Coverage', 10, 20),
+(3, 'Full Coverage', 2, 0),
+(4, 'Low Coverage', 5, 7),
+(5, 'Mega Coverage', 0, 80),
+(6, 'Mega Coverage', -1, 80),
+(7, 'Special Full Coverage', 15, 20),
+(9, 'Special Full Coverage', 10, 49),
+(10, 'Special Full Coverage', 5, 49),
+(11, 'Super Sale', 3, 6);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
